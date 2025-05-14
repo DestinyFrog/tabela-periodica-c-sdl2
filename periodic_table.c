4665e1d6c17b4682fe391fc4d80c74bc93d0af0b
DestinyFrog <pedrocalizto9@gmail.com>
2025-05-14 16:39:50 -0300

    add atomic_number

--- periodic_table.c
+++ periodic_table.c
@@ -12,20 +12,31 @@ SDL_Window* window;
 SDL_Renderer* render;
 SDL_Event window_event;
 
-int border = 10,
-    unit = 36,
-    text_unit = 28,
-    gap = 2,
+TTF_Font *courier_new_24 = NULL,
+         *courier_new_12 = NULL;
+
+const unsigned int
+    border = 10,
+    unit = 64,
+    text_unit_12 = 14,
+    text_unit_24 = 28,
+    gap = 2;
+
+unsigned short int
+    i, j,
     width,
     height,
-    length,
-    i, j;
-
-TTF_Font* font = NULL;
+    length;
 
 void awake() {
-    font = TTF_OpenFont("courier_new.ttf", 24);
-    if(!font) {
+    courier_new_24 = TTF_OpenFont("courier_new.ttf", 24);
+    if(!courier_new_24) {
+        printf("TTF_OpenFont: %s\n", TTF_GetError());
+        exit(EXIT_FAILURE);
+    }
+
+    courier_new_12 = TTF_OpenFont("courier_new.ttf", 12);
+    if(!courier_new_12) {
         printf("TTF_OpenFont: %s\n", TTF_GetError());
         exit(EXIT_FAILURE);
     }
@@ -35,21 +46,20 @@ void awake() {
 }
 
 SDL_Rect r;
-SDL_Rect text_r;
 
 SDL_Surface* text_surface;
 SDL_Texture* text_texture;
 SDL_Color color;
 
+char str_atomic_number[4];
+
 void draw() {
     SDL_SetRenderDrawColor( render, 0, 0, 0, 255 );
     SDL_RenderClear( render );
 
-    r.w = unit;
-    r.h = unit;
-    text_r.h = text_unit;
-
     for (i = 0; i < ELEMENT_COUNT; i++) {
+        r.w = unit;
+        r.h = unit;
         r.x = border + elements[i].x * (unit + gap);
         r.y = border + elements[i].y * (unit + gap);
 
@@ -59,15 +69,31 @@ void draw() {
         SDL_SetRenderDrawColor( render, color.r, color.g, color.b, color.a );
 		SDL_RenderDrawRect( render, &r );
 
-        for (length = 0; elements[i].symbol[length] != '\0'; length++);
-        
-        text_r.w = text_unit/2 * length;
-        text_r.x = r.x + unit/2 - (text_unit/2) + (length == 1 ? (text_r.w/2) : 0);
-        text_r.y = r.y + unit/2 - (text_unit/2);
+        sprintf(str_atomic_number, "%hi", i + 1);
 
-        text_surface = TTF_RenderText_Solid( font, elements[i].symbol, color );
+        for (length = 0; str_atomic_number[length] != '\0'; length++);
+
+        r.w = text_unit_12/2 * length;
+        r.h = text_unit_12;
+        r.x = r.x + 2;
+        r.y = r.y + 2;
+
+        text_surface = TTF_RenderText_Solid( courier_new_12, str_atomic_number, color );
         text_texture = SDL_CreateTextureFromSurface( render, text_surface );
-        SDL_RenderCopy( render, text_texture, NULL, &text_r );
+        SDL_RenderCopy( render, text_texture, NULL, &r );
+        SDL_DestroyTexture( text_texture );
+        SDL_FreeSurface( text_surface );
+
+        for (length = 0; elements[i].symbol[length] != '\0'; length++);
+
+        r.w = text_unit_24/2 * length;
+        r.h = text_unit_24;
+        r.x = r.x + unit/2 - (text_unit_24/2) + (length == 1 ? (r.w/2) : 0);
+        r.y = r.y + unit/2 - (text_unit_24/2);
+
+        text_surface = TTF_RenderText_Solid( courier_new_24, elements[i].symbol, color );
+        text_texture = SDL_CreateTextureFromSurface( render, text_surface );
+        SDL_RenderCopy( render, text_texture, NULL, &r );
         SDL_DestroyTexture( text_texture );
         SDL_FreeSurface( text_surface );
     }
@@ -102,7 +128,7 @@ int main() {
         SDL_Delay(10);
     }   while ( SDL_QUIT != window_event.type );
 
-    TTF_CloseFont(font);
+    TTF_CloseFont(courier_new_24);
     TTF_Quit();
 
     SDL_DestroyWindow( window );
